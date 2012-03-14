6e9fbf1b7db62db11021e4df50362adb79f693b2
Steve Kondik <shade@chemlab.org>
2012-03-14 00:10:33 -0700

    cm: New boot animation from Cameron Behzadpour
     * Default for tablet is horizontal-1280x800
     * Default for phone is vertical-480x800
     * Override using TARGET_BOOTANIMATION_NAME in cm.mk (must come before
       inherit of CM base product.
    
    Change-Id: Ibd01236847b4f1de0cfd4246c8bd93b1f047005a

--- config/common.mk
+++ config/common.mk
@@ -1,5 +1,10 @@
 PRODUCT_BRAND ?= cyanogenmod
 
+ifneq ($(TARGET_BOOTANIMATION_NAME),)
+    PRODUCT_COPY_FILES += \
+        vendor/cm/prebuilt/common/bootanimation/$(TARGET_BOOTANIMATION_NAME).zip:system/media/bootanimation.zip
+endif
+
 ifdef CM_NIGHTLY
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.rommanager.developerid=cyanogenmodnightly
@@ -45,7 +50,6 @@ PRODUCT_COPY_FILES +=  \
     vendor/cm/proprietary/RomManager.apk:system/app/RomManager.apk \
     vendor/cm/proprietary/Term.apk:system/app/Term.apk \
     vendor/cm/proprietary/lib/armeabi/libjackpal-androidterm3.so:system/lib/libjackpal-androidterm3.so \
-    vendor/cm/prebuilt/common/bootanimation.zip:system/media/bootanimation.zip \
     vendor/cm/prebuilt/common/bin/fix_permissions:system/bin/fix_permissions
 
 # Bring in camera effects & videos
